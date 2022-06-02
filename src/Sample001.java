4697de517b7704a1fefac66ac064928c1ef852e1
takenokonyokki <97602717+takenokonyokki@users.noreply.github.com>
2022-06-02 22:42:50 +0900

    javaの実行確認

--- src/Sample001.java
+++ src/Sample001.java
@@ -1,5 +1,13 @@
 public class Sample001 {
 	public static void main (String[] args) {
-		System.out.println("Hello Wrold");
+		System.out.println("javajava");
+		System.out.println("27 + 27");
+		System.out.println(27 + 27);
+		System.out.println(35 - 23);
+		
+//		変数を定義
+		int x; //変数xを定義
+		x = 8; //変数に８を代入
+		System.out.println(x * x * 3.14); //変数を使って計算
 	}
 }
